0afc71ab24e19ed904321a60bc07dbc42ef41dc3
Anthony Jimenez <a.fabricio2003@gmail.com>
2024-09-17 03:02:23 -0600

    File config

--- config.php
+++ config.php
@@ -1,5 +1,10 @@
 <?php
 // Archivo: config.php
+
+if (php_sapi_name() !== 'cli' && strpos($_SERVER['REQUEST_URI'], basename(__FILE__)) !== false) {
+    die('No tienes acceso a este archivo.');
+}
+
 return [
     'db' => [
         'host' => 'localhost',
